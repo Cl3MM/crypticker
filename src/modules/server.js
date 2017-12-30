aa9cdb21130aa19e44f2ab9ad3b249873dcfb43f
Clement Wheelmilk <clement.roullet@gmail.com>
2017-12-30 20:30:36 +0100

    Added ejson, added mongoose

--- src/modules/server.js
+++ src/modules/server.js
@@ -2,17 +2,20 @@ const moment = require('moment')
 const express = require('express')
 const Primus = require('primus')
 const http = require('http')
-const MongoDb = require('./persistence')
-const Instrument = require('./inst')
+const MongoDb = require('./mongo')
+const Plugins = require('../plugins/loader')
 
-module.exports = class Server {
+class Server {
   constructor (opts) {
     this.app = express()
     this.app.get('/', this.serve.bind(this))
     this.port = process.env.WS_PORT || 3000
     this.server = http.createServer(this.app)
-    this.primus = new Primus(this.server, { transformer: 'websockets' })
+    this.primus = new Primus(this.server, { transformer: 'websockets', parser: 'EJSON' })
     this.db = new MongoDb()
+    this.plugins = new Plugins().load({
+      save: i => i.save()
+    })
 
     return this
   }
@@ -24,7 +27,9 @@ module.exports = class Server {
       .catch(e => res.status(500).json(e))
   }
 
-  run () {
+  start () {
+    this.plugins.start()
+
     this.primus.on('connection', (spark) => {
       spark.on('data', (data) => {
         if (data && data.action && data.action === 'tick') {
@@ -43,3 +48,5 @@ module.exports = class Server {
     return this
   }
 }
+
+module.exports = Server
