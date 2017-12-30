44f823a819450b1ff713adf8750863829b2a804a
Clement Wheelmilk <clement.roullet@gmail.com>
2017-12-30 20:36:44 +0100

    Use mongoose schema to define instrument

---
+++ src/models/instrument.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const options = {
+  discriminatorKey: 'kind',
+  autoIndex: true
+}
+
+const instrumentSchema = new mongoose.Schema({
+  mkt: { type: String, index: true },
+  cur: { type: String, index: true },
+  isoCur: String,
+  oCur: String,
+  time: { type: Date, index: true },
+  bid: Number,
+  ask: Number,
+  volume: Number,
+  price: Number,
+  high: Number,
+  low: Number,
+}, options)
+
+const Instrument = mongoose.model('Ticker', instrumentSchema)
+
+module.exports = Instrument
